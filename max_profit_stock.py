0b4d695a7103687e6c317acb14ba01953c59f4d1
Girish Gupta <67008135+girishgupta211@users.noreply.github.com>
2020-07-02 18:28:51 +0530

    Create max_profit_stock.py

---
+++ max_profit_stock.py
@@ -0,0 +1,27 @@
+def find_max_profit(prices):
+    lowest = prices[0]
+    max_profit = 0
+    for i in range(1, len(prices)):
+
+        current_profit = prices[i] - lowest
+
+        if current_profit > max_profit:
+            max_profit = current_profit
+            print("sell price ", prices[i])
+            print("buy price ", lowest)
+            print("max_profit ", max_profit)
+
+        if prices[i] < lowest:
+            lowest = prices[i]
+
+    return max_profit
+
+
+stock_prices = [
+    [7, 1, 5, 3, 4, 6, 2],
+    [7, 7, 7],
+    [7, 6, 5, 4],
+    [1, 2, 3, 4, 5]]
+
+for stock_price in stock_prices:
+    find_max_profit(stock_price)
